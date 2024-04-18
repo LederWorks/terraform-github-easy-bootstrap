b3132aebe87654ad5a6929ccaa264256dbe53a27
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-04-18 08:59:17 +0200

    kosh

--- inputs.tf
+++ inputs.tf
@@ -1,11 +1,10 @@
 #Variables
-#Variables
 resource "github_actions_variable" "variable" {
   for_each = local.variables
 
   repository    = github_repository.repo[each.key].id
   variable_name = each.key
-  value         = each.value
+  value         = each.value["desired_key"] # replace "desired_key" with the actual key
 }
 
 #Secrets
@@ -15,12 +14,12 @@ resource "github_actions_secret" "secret" {
   repository      = github_repository.repo[each.key].id
   secret_name     = each.key
   # encrypted_value = each.value
-  plaintext_value = each.value
+  plaintext_value = each.value["desired_key"] # replace "desired_key" with the actual key
 }
 
 #Labels
 resource "github_issue_label" "labels" {
-   for_each = toset(flatten([
+   for_each = { for item in flatten([
     for repo_key, repo in var.repos : [
       for label_key, label in (repo.custom_labels != null ? repo.custom_labels : {}) : {
         repo_key    = repo_key
@@ -28,11 +27,10 @@ resource "github_issue_label" "labels" {
         label_value = label
       }
     ]
-  ]))
+  ]) : item.repo_key => item } # creating a map with repo_key as the key and the object as the value
 
   repository  = each.value.repo_key
   name        = each.value.label_value.name
   description = each.value.label_value.description
   color       = each.value.label_value.color
 }
-
