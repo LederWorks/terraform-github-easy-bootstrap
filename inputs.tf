b042bf218d05d2a7060dc41470c4171411b01ddd
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-04-18 07:29:32 +0200

    posh

--- inputs.tf
+++ inputs.tf
@@ -1,33 +1,36 @@
 #Variables
 resource "github_actions_variable" "variable" {
-  for_each = local.variables != {} ? local.variables : {}
+  for_each = local.variables[github_repository.repo[each.key].name] 
 
   repository    = github_repository.repo[each.key].id
   variable_name = each.key
-  value         = each.value.value
+  value         = each.value
 }
 
 #Secrets
 resource "github_actions_secret" "secret" {
-  for_each = local.secrets != {} ? local.secrets : {}
+  for_each = local.secrets[github_repository.repo[each.key].name] 
 
   repository      = github_repository.repo[each.key].id
   secret_name     = each.key
   # encrypted_value = each.value
-  plaintext_value = each.value.value
+  plaintext_value = each.value
 }
 
 #Labels
 resource "github_issue_label" "labels" {
-  for_each = {
-    for repo_key, repo in var.repos : repo_key => {
-      for label_key, label in (repo.custom_labels != null ? repo.custom_labels : {}) :
-      label_key => label
-    }
-  }
+   for_each = toset(flatten([
+    for repo_key, repo in var.repos : [
+      for label_key, label in (repo.custom_labels != null ? repo.custom_labels : {}) : {
+        repo_key    = repo_key
+        label_key   = label_key
+        label_value = label
+      }
+    ]
+  ]))
 
-  repository  = each.key
-  name        = each.value.name
-  description = each.value.description
-  color       = each.value.color
+  repository  = each.value.repo_key
+  name        = each.value.label_value.name
+  description = each.value.label_value.description
+  color       = each.value.label_value.color
 }
