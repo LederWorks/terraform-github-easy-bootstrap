1c9f43f33835a4ca9e019c9f6b0a80216619bc29
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-04-18 14:58:26 +0200

    positif

--- labels.tf
+++ labels.tf
@@ -10,10 +10,11 @@ locals {
   flattened_labels = flatten([
     for repo_key, labels in local.labels : [
       for label_name, label in labels : {
-        repo_key          = repo_key
+        repo_name          = repo_key
         label_name        = label_name
-        label_color       = label.color
-        label_description = label.description
+        label_color       = label_color
+        label_description = label_description
+        repository_id = github_repository.repo[repo_key].id
       }
     ]
   ])
@@ -30,27 +31,9 @@ output "flattened_labels" {
 }
 
 resource "github_issue_label" "label" {
-  for_each = { for label in local.flattened_labels : "${label.repo_key}_${label.label_name}" => label }
-
-  repository  = each.value.repo_key
+  for_each = { for label in local.flattened_labels : "${label.repo_name}_${label.label_name}" => label }
+  repository  = each.value.repository_id
   name        = each.value.label_name
   color       = each.value.label_color
   description = each.value.label_description
 }
-
-/* resource "github_issue_label" "labels" {
-   for_each = { for item in flatten([
-    for repo_key, repo in var.repos : [
-      for label_key, label in (repo.custom_labels != null ? repo.custom_labels : {}) : {
-        repo_key    = repo_key
-        label_key   = label_key
-        label_value = label
-      }
-    ]
-  ]) : item.repo_key => item } # creating a map with repo_key as the key and the object as the value
-
-  repository  = each.value.repo_key
-  name        = each.value.label_value.name
-  description = each.value.label_value.description
-  color       = each.value.label_value.color
-} */
